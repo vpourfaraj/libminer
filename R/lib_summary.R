d14a5fd65cc8b9fd8f3391324289ebc7e3af61c8
Vahab Pourfaraj <vpourfaraj@gmail.com>
2024-11-05 12:00:29 -0800

    Factored out lib and calculated sizes

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -1,8 +1,11 @@
-#' Provide the number of R Packages
+#' Provide Number of R Packages by Library
 #'
-#' @param sizes Should sizes of libraries be calculated. Default is "FALSE".
+#' Provide the number of R package by library in
+#'  a data.frame
 #'
-#' @return a data.frame  of R packages
+#' @param sizes Should sizes of libraries be calculated. Default `FALSE`.
+#'
+#' @return a data.frame of R packages by library
 #' @export
 #'
 #' @examples
@@ -11,16 +14,39 @@ lib_summary <- function(sizes = FALSE) {
   if (!is.logical(sizes)) {
     stop("'sizes' must be logical (TRUE or FALSE)")
   }
-  pkgs <- utils::installed.packages()
-  pkg_tbl <- table(pkgs[, "LibPath"])
+
+  pkg_df <- lib()
+  pkg_tbl <- table(pkg_df[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+
   names(pkg_df) <- c("Library", "n_packages")
 
   if (isTRUE(sizes)) {
-    pkg_df$lib_size <- map_dbl(
-      pkg_df$Library,
-        \(x) sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
-    )
+    pkg_df <- calculate_sizes(pkg_df)
   }
+
   pkg_df
+}
+
+#' Generate a data frame of installed packages
+#'
+#' @return a data.frame of all packages installed on your system
+#' @export
+lib <- function() {
+  pkgs <- utils::installed.packages()
+  as.data.frame(pkgs, stringsAsFactors = FALSE)
+}
+
+#' calculate sizes
+#'
+#' @param df a data.frame
+#'
+#' @return df with a lib_size column
+#' @noRd
+calculate_sizes <- function(df) {
+  df$lib_size <- map_dbl(
+    df$Library,
+    \(x) sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+  )
+  df
 }
