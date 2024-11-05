97d47155dc5cda7b5ad1f3ade2d1475fdd36428f
Vahab Pourfaraj <vpourfaraj@gmail.com>
2024-11-05 09:40:23 -0800

    minor edits

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -7,7 +7,7 @@
 #' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages() #what packages are installed on our system
-  pkg_tbl <- table(pkgs[, "LibPath"]) #summarises the number of rows
+  pkg_tbl <- table(pkgs[, "LibPath"]) #summarizes the number of rows
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE) #turn to df
   names(pkg_df) <- c("Library", "n_packages")
   pkg_df
