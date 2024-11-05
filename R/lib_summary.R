6cfaf483c23a41b2b2444bd133b8517664d2ac59
Vahab Pourfaraj <vpourfaraj@gmail.com>
2024-11-05 10:30:17 -0800

    add dependency and sizes function

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -1,14 +1,29 @@
 #' Provide the number of R Packages
 #'
+#' @param sizes Should sizes of libraries be calculated. Default is "FALSE".
+#'
 #' @return a data.frame  of R packages
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
-  pkgs <- utils::installed.packages() #what packages are installed on our system
-  pkg_tbl <- table(pkgs[, "LibPath"]) #summarizes the number of rows
-  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE) #turn to df
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be logical (TRUE or FALSE)")
+  }
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (isTRUE(sizes)) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
   pkg_df
 }
--- tests/testthat/test-lib_summary.R
+++ tests/testthat/test-lib_summary.R
@@ -9,6 +9,16 @@ test_that("lib_summary returns expected results", {
   expect_type(result$Library, "character")
 })
 
+test_that("sizes works", {
+  result <- lib_summary(size = TRUE)
+  expect_s3_class(result, "data.frame")
+  expect_equal(ncol(result), 3)
+  expect_gte(nrow(result), 1)
+  expect_equal(names(result), c("Library", "n_packages", "lib_size"))
+  expect_type(result$n_packages, "integer")
+  expect_type(result$Library, "character")
+  expect_type(result$lib_size, "double")
+})
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("hello"), "unused argument")
+  expect_error(lib_summary("hello"), "must be logical")
 })
